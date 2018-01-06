4134a61bee3273a945ef52bd64602524c76df045
ArtificialSloth <artificialsloth@gmail.com>
2018-01-06 02:35:31 +0000

    Database now connects to a URL specified in the MONGODB_URL env variable.

--- api/database/db.js
+++ api/database/db.js
@@ -2,7 +2,7 @@ module.exports = (crp, callback) => {
 	var mongodb = require('mongodb');
 	var mongoClient = mongodb.MongoClient;
 	
-	mongoClient.connect('mongodb://localhost:27017/chroniclesrp', (err, db) => {
+	mongoClient.connect(process.env.MONGODB_URL, (err, db) => {
 		if (err) throw err;
 		
 		db.PREFIX = 'CRP_';
