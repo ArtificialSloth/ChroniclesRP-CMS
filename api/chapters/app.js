0c726f30e96a28991121bbc8567f00cec47b405e
ArtificialSloth <artificialsloth@gmail.com>
2018-08-10 17:30:45 -0400

    Disabled non-working CMS options in Chapter creation and now only registering routes for hosted chapters.

--- api/chapters/app.js
+++ api/chapters/app.js
@@ -204,7 +204,9 @@ module.exports = (crp, callback) => {
 
 	crp.util.getChapters({}, (err, chapters) => {
 		for (var i in chapters) {
-			crp.proxy.register(chapters[i].slug + '.' + (process.env.DOMAIN || 'chroniclesrp.com'), '127.0.0.1:8080', {ssl: crp.ssl});
+			if (chapters[i].type == 'hosted') {
+				crp.proxy.register(chapters[i].slug + '.' + (process.env.DOMAIN || 'chroniclesrp.com'), '127.0.0.1:8080', {ssl: crp.ssl});
+			}
 		}
 
 		callback();
