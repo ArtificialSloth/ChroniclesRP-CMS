95ce24bfbcee5e347d8c9746366d4e37d9db4c6a
ArtificialSloth <artificialsloth@gmail.com>
2018-10-27 18:57:41 -0400

    The Discord API will now try several times to send a message before failing. Board members can manually send an inquery using the '/message <USERID>' command.

--- api/discord/discord.js
+++ api/discord/discord.js
@@ -3,12 +3,22 @@ module.exports = (crp, callback) => {
 	crp.discord = new Eris(process.env.DISCORD_TOKEN);
 
 	crp.discord.inqueries = {};
-	crp.discord.send = (msg, channel) => {
+	crp.discord.send = (msg, channel, cb) => {
 		var channel = channel || crp.discord.getChannel(process.env.DISCORD_CHANNEL);
-		return channel ? channel.createMessage(msg) : false;
+		if (!channel) return;
+
+		crp.async.retry({times: 4, interval: 500}, (callback) => {
+			channel.createMessage(msg).then((msg) => {
+				callback(null, msg);
+			}).catch(callback);
+		}, (err, result) => {
+			if (cb) return cb(err, result);
+		});
 	};
 
-	crp.discord.on('guildMemberAdd', (guild, member) => {
+	crp.discord.inquire = (guild, member, errChannel) => {
+		if (!guild || !member) return;
+
 		crp.discord.getDMChannel(member.id).then((channel) => {
 			crp.db.findOne('site', {}, (err, site) => {
 				if (err) return;
@@ -24,23 +34,31 @@ module.exports = (crp, callback) => {
 				}
 
 				var msg = `${site.discord.greeting}\n\n**${inquery.question}**${answers}`;
-				crp.discord.send(msg, channel);
+				crp.discord.send(msg, channel, (err, result) => {
+					if (err && errChannel) return crp.discord.send(`\`\`\`${err}\`\`\``, errChannel);
 
-				inquery.guild = guild;
-				crp.discord.inqueries[member.id] = inquery;
+					inquery.guild = guild;
+					crp.discord.inqueries[member.id] = inquery;
+				});
 			});
 		}).catch(console.error);
-	});
+	};
 
+	crp.discord.on('guildMemberAdd', crp.discord.inquire);
 	crp.discord.on('messageCreate', (msg) => {
+		if (msg.content.startsWith('/message')) {
+			if (!msg.member || !msg.member.roles.includes(process.env.DISCORD_BOARD_ROLE)) return;
+
+			return crp.discord.inquire(msg.member.guild, {id: msg.content.replace('/message ', '')}, msg.channel);
+		}
+
 		if (!msg.channel.recipient) return;
 
 		var inquery = crp.discord.inqueries[msg.channel.recipient.id];
 		if (!inquery) return;
 
 		var res = parseInt(msg.content);
-		if (!res || res < 1 || res > inquery.choices.length) return
-
+		if (!res || res < 1 || res > inquery.choices.length) return;
 
 		crp.db.findOne('site', {}, (err, site) => {
 			if (err) return;
@@ -48,7 +66,7 @@ module.exports = (crp, callback) => {
 			crp.discord.inqueries[msg.channel.recipient.id] = null;
 			if (inquery.choices[res - 1] != inquery.answer) return crp.discord.send(site.discord.reject, msg.channel);
 
-			crp.discord.addGuildMemberRole(inquery.guild.id, msg.channel.recipient.id, process.env.DISCORD_ROLE).then(() => {
+			crp.discord.addGuildMemberRole(inquery.guild.id, msg.channel.recipient.id, process.env.DISCORD_DEFAULT_ROLE).then(() => {
 				crp.discord.send(site.discord.accept, msg.channel);
 			}).catch(console.error);
 		});
@@ -56,6 +74,9 @@ module.exports = (crp, callback) => {
 
 	crp.discord.connect();
 	crp.discord.on('ready', () => {
+		if (!callback) return;
+
 		callback(null, crp);
+		callback = null;
 	});
 };
