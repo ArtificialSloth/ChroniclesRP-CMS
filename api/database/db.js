a789d236384d83bb43f96180ca1111b8e2193ea6
ArtificialSloth <artificialsloth@gmail.com>
2019-04-04 19:01:41 -0400

    Added games model to database API.

--- api/database/db.js
+++ api/database/db.js
@@ -31,53 +31,24 @@ module.exports = (crp, callback) => {
 			}
 		}));
 
+		crp.games = crp.db.model('game', new crp.db.Schema({
+			name: {
+				type: String,
+				required: true
+			},
+			slug: {
+				type: String,
+				lowercase: true,
+				default: function() {
+					return crp.util.urlSafe(this.name);
+				}
+			},
+			icon: {
+				type: String,
+				required: true
+			}
+		}));
+
 		callback(err, crp);
 	});
-
-	/*var mongodb = require('mongodb');
-	var mongoClient = mongodb.MongoClient;
-
-	mongoClient.connect(process.env.MONGODB_URL, (err, db) => {
-		if (err) return callback(err, crp);
-
-		crp.db = {
-			mongodb: db,
-			prefix: 'CRP_',
-			sanitize: require('mongo-sanitize')
-		};
-
-		crp.db.find = (collection, filter, options, cb) => {
-			crp.db.mongodb.collection(crp.db.prefix + collection).find(filter, options).toArray(cb);
-		};
-
-		crp.db.findOne = (collection, filter, cb) => {
-			crp.db.mongodb.collection(crp.db.prefix + collection).findOne(filter, cb);
-		};
-
-		crp.db.insertOne = (collection, data, cb) => {
-			crp.db.mongodb.collection(crp.db.prefix + collection).insertOne(data, cb);
-		};
-
-		crp.db.replaceOne = (collection, filter, data, cb) => {
-			crp.db.mongodb.collection(crp.db.prefix + collection).replaceOne(filter, data, cb);
-		};
-
-		crp.db.deleteOne = (collection, filter, cb) => {
-			crp.db.mongodb.collection(crp.db.prefix + collection).deleteOne(filter, cb);
-		};
-
-		crp.db.deleteMany = (collection, filter, cb) => {
-			crp.db.mongodb.collection(crp.db.prefix + collection).deleteMany(filter, cb);
-		};
-
-		crp.db.objectID = (oid) => {
-			try {
-				return mongodb.ObjectId(oid);
-			} catch (e) {
-				return e;
-			}
-		};
-
-		callback(null, crp);
-	});*/
 };
