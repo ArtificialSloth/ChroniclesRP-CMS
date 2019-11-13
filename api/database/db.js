2952684ffd33cb745f9dbf1fdda1b263595ece7e
ArtificialSloth <artificialsloth@gmail.com>
2019-11-12 21:17:14 -0500

    Updated outdated packages and fixed vulnerabilities.

--- api/database/db.js
+++ api/database/db.js
@@ -1,7 +1,7 @@
 module.exports = (crp, callback) => {
 	crp.db = require('mongoose');
 
-	crp.db.connect(process.env.MONGODB_URL, {useNewUrlParser: true}, (err) => {
+	crp.db.connect(process.env.MONGODB_URL, {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
 		crp.sites = crp.db.model('site', new crp.db.Schema({
 			name: {type: String, required: true},
 			tagline: String,
